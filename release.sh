3d5afb4fe93c01529ceedb8940efc908702d48cf
Jean M. Lescure <jeanmlescure@gmail.com>
2020-10-16 12:46:06 -0600

    chore: ignore personal release script file

--- release.sh
+++
@@ -1,9 +0,0 @@
-
-npm login
-npm publish
-npm pack
-git tag -a v$(jq -r .version package.json) -m "Releasing version v$(jq -r .version package.json)"
-git push origin --tags
-npm adduser --registry=https://npm.pkg.github.com/
-read -n 1 -s -r -p "Add @ to package.json and press any key to continue"
-npm publish --registry=https://npm.pkg.github.com/
