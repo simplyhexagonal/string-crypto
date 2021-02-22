00291b1738cafa0f9aff3034795efc32f642e12d
Jean M. Lescure <jeanmlescure@gmail.com>
2021-02-21 23:26:54 -0600

    feat: refactor out external packages

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 declare type StringLike = string | Buffer | Uint8Array | Int8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | DataView;
-declare type Digest = 'md5' | 'sha1' | 'sha224' | 'sha256' | 'sha384' | 'sha512' | 'rmd160' | 'ripemd160';
+declare type Digest = 'blake2b512' | 'blake2s256' | 'md4' | 'md5' | 'md5-sha1' | 'mdc2' | 'ripemd160' | 'sha1' | 'sha224' | 'sha256' | 'sha3-224' | 'sha3-256' | 'sha3-384' | 'sha3-512' | 'sha384' | 'sha512' | 'sha512-224' | 'sha512-256' | 'sm3' | 'whirlpool';
 interface DeriveKeyOpts {
     salt?: StringLike;
     iterations?: number;
